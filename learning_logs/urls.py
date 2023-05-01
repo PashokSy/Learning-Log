71d52510a2c98596779ca8a15b89c799f8147357
PavelsPC <pavlo.syzonenko@gmail.com>
2023-05-01 21:18:14 +0300

    Create Main Page

---
+++ learning_logs/urls.py
@@ -0,0 +1,11 @@
+"""Define URL patterns for learning_logs."""
+
+from django.urls import path
+from . import views
+
+app_name = 'learning_logs'
+urlpatterns = [
+    # Home page
+    path('', views.index, name='index'),
+    path('topics/', views.topics, name='topics'),
+]
--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
+from learning_logs.models import Topic
+
+
 # Create your views here.
+def index(request):
+    """The home page for Learning Log."""
+    return render(request, 'learning_logs/index.html')
+
+
+# TODO fix topics list is empty
+def topics(request):
+    """Show all topics."""
+    topics = Topic.objects.order_by('date_added')
+    context = {'topic': topics}
+    return render(request, 'learning_logs/topics.html', context)
