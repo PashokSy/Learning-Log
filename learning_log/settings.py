459df6a5fbb56f2e9182ab40840fdc8fb4036ef5
PavelsPC <pavlo.syzonenko@gmail.com>
2023-05-01 18:49:09 +0300

    Change main app name

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'learning_log_app.apps.LearningLogAppConfig',
+    'learning_logs.apps.LearningLogAppConfig',
 ]
 
 MIDDLEWARE = [
--- learning_logs/apps.py
+++ learning_logs/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class LearningLogAppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'learning_log_app'
+    name = 'learning_logs'
