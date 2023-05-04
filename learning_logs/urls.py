a69a33b5b4308c567eb357773540bc0a5355d7ec
PavelsPC <pavlo.syzonenko@gmail.com>
2023-05-04 18:53:09 +0300

    Create Add new topic functionality

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -5,9 +5,12 @@ from . import views
 
 app_name = 'learning_logs'
 urlpatterns = [
-    # Home page
+    # Home page.
     path('', views.index, name='index'),
+    # Page for list of topics.
     path('topics/', views.topics, name='topics'),
     # Detail page for a single topic.
     path('topics/<int:topic_id>', views.topic, name='topic'),
+    # Page for adding a new topic.
+    path('new_topic/', views.new_topic, name='new_topic'),
 ]
--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from learning_logs.models import Topic
+from learning_logs.forms import TopicForm
 
 
 def index(request):
@@ -22,3 +23,19 @@ def topic(request, topic_id):
     context = {'topic': topic, 'entries': entries}
     return render(request, 'learning_logs/topic.html', context)
 
+
+def new_topic(request):
+    """Add a new topic."""
+    if request.method != 'POST':
+        # No data submitted; create a blank form.
+        form = TopicForm()
+    else:
+        # POST data submitted; process data.
+        form = TopicForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('learning_logs:topics')
+
+    # Display a blank of invalid form
+    context = {'form': form}
+    return render(request, 'learning_logs/new_topic.html', context)
