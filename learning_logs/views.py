78347237ac0e114b3a17954306867443c7f04e84
PavelsPC <pavlo.syzonenko@gmail.com>
2023-05-04 19:36:47 +0300

    Create Edit entry functionality

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 
-from learning_logs.models import Topic
+from learning_logs.models import Topic, Entry
 from learning_logs.forms import TopicForm, EntryForm
 
 
@@ -60,3 +60,22 @@ def new_entry(request, topic_id):
     # Display a blank or invalid form
     context = {'topic': topic, 'form': form}
     return render(request, 'learning_logs/new_entry.html', context)
+
+
+def edit_entry(request, entry_id):
+    """Edit an existing entry."""
+    entry = Entry.objects.get(id=entry_id)
+    topic = entry.topic
+
+    if request.method != 'POST':
+        # Initial request; pre-fill form with the current entry.
+        form = EntryForm(instance=entry)
+    else:
+        # POST data submitted; process data.
+        form = EntryForm(instance=entry, data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('learning_logs:topic', topic_id = topic.id)
+
+    context = {'entry': entry, 'topic': topic, 'form': form}
+    return render(request, 'learning_logs/edit_entry.html', context)
