229c578769dcc32347ab2e5dc2db4acb6c192c2d
PavelsPC <pavlo.syzonenko@gmail.com>
2023-05-04 18:15:16 +0300

    Finish and fix Topics page Create single Topic page

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     # Home page
     path('', views.index, name='index'),
     path('topics/', views.topics, name='topics'),
+    # Detail page for a single topic.
+    path('topics/<int:topic_id>', views.topic, name='topic'),
 ]
--- learning_logs/views.py
+++ learning_logs/views.py
@@ -3,15 +3,22 @@ from django.shortcuts import render
 from learning_logs.models import Topic
 
 
-# Create your views here.
 def index(request):
     """The home page for Learning Log."""
     return render(request, 'learning_logs/index.html')
 
 
-# TODO fix topics list is empty
 def topics(request):
     """Show all topics."""
     topics = Topic.objects.order_by('date_added')
-    context = {'topic': topics}
+    context = {'topics': topics}
     return render(request, 'learning_logs/topics.html', context)
+
+
+def topic(request, topic_id):
+    """Show single topic and all his entry."""
+    topic = Topic.objects.get(id=topic_id)
+    entries = topic.entry_set.order_by('-date_added')
+    context = {'topic': topic, 'entries': entries}
+    return render(request, 'learning_logs/topic.html', context)
+
